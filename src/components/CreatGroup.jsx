0c97aa7c38577c80c5540a15282526c29e853895
Fazla rabbi <fazlarabbi911@gmail.com>
2024-09-01 18:27:15 +0600

    .

---
+++ src/components/CreatGroup.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CreatGroup = () => {
+  return (
+    <div className='flex space-x-[90%]  '>
+      <div>z</div>
+      <div>z</div>
+    </div>
+  )
+}
+
+export default CreatGroup
--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import CreatGroup from '../components/CreatGroup'
 
 const Feed = () => {
   return (
-    <div className='h-screen'>
-      <h2 className='text-white'>feed</h2>
+    // <div className='h-screen'>
+    //   <h2 className='text-white'>feed</h2>
+    // </div>
+    <div class="flex flex-wrap gap-3 my-5 h-[90%]" >
+    <div className='bg-[#242c59e0] w-[32%] h-[49%] rounded-[10px] text-white'>
+      <CreatGroup/>
     </div>
+    <div className='bg-[#242c59e0] w-[32%] h-[49%] rounded-[10px] text-white'>
+
+    </div>
+    <div className='bg-[#242c59e0] w-[32%] h-[49%] rounded-[10px] text-white'></div>
+    <div className='bg-[#242c59e0] w-[32%] h-[49%] rounded-[10px] text-white'></div>
+    <div className='bg-[#242c59e0] w-[32%] h-[49%] rounded-[10px] text-white'></div>
+    <div className='bg-[#242c59e0] w-[32%] h-[49%] rounded-[10px] text-white'></div>
+  </div>
+    
   )
 }
 
