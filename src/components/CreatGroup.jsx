82cb1d4b18b461125742fb37c93f92a7f8978c0f
Fazla rabbi <fazlarabbi911@gmail.com>
2024-09-10 22:46:21 +0600

    box

--- src/components/CreatGroup.jsx
+++ src/components/CreatGroup.jsx
@@ -1,10 +1,54 @@
 import React from 'react'
+import Image from './Image'
 
 const CreatGroup = () => {
   return (
-    <div className='flex space-x-[90%]  '>
-      <div>z</div>
-      <div>z</div>
+    <div className='overflow-y-auto h-[90%]'>
+      <h2 className='font-mono font-bold text-[24px] text-[#c9c2ff] bg-[#272d9859] p-2'>Title </h2>
+      <div className=' overflow-hidden'>
+        <div className=' flex justify-between p-2 items-center h-22 bg-[#2f357692] rounded-[20px] border-[1px] border-[#f1f1f137]'>
+          <div className=' w-[20%] rounded-lg '><img className="rounded-[30px]" src="https://firebasestorage.googleapis.com/v0/b/cootalk-e6218.appspot.com/o/profile-pwwAPPoO7CR3b9JkUPwTeSJjpSD3?alt=media&token=430d4cb2-6212-4040-bf04-1e0d0291ef43"/></div>
+          <div className=' w-[60%] text-left pl-8 '>
+            <h2 className='font-mono font-bold text-[20px] text-[#c9c2ff]'>Fazla Rabbi</h2>
+            <p className='font-mono font-bold text-[14px] text-[#c9c2ff] '>hi there</p>
+          </div>
+          <div  className=' w-[20%]'><button className=' p-2 rounded-lg font-bold text-[16px] transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 border-[1px] border-[#f1f1f160]'>Click</button></div>
+        </div>
+        <div className=' flex justify-between p-2 items-center h-22 bg-[#2f357692] rounded-[20px] border-[1px] border-[#f1f1f137]'>
+          <div className=' w-[20%] rounded-lg '><img className="rounded-[30px]" src="https://firebasestorage.googleapis.com/v0/b/cootalk-e6218.appspot.com/o/profile-pwwAPPoO7CR3b9JkUPwTeSJjpSD3?alt=media&token=430d4cb2-6212-4040-bf04-1e0d0291ef43"/></div>
+          <div className=' w-[60%] text-left pl-8 '>
+            <h2 className='font-mono font-bold text-[20px] text-[#c9c2ff]'>Fazla Rabbi</h2>
+            <p className='font-mono font-bold text-[14px] text-[#c9c2ff] '>hi there</p>
+          </div>
+          <div  className=' w-[20%]'><button className=' p-2 rounded-lg font-bold text-[16px] transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 border-[1px] border-[#f1f1f160]'>Click</button></div>
+        </div>
+        <div className=' flex justify-between p-2 items-center h-22 bg-[#2f357692] rounded-[20px] border-[1px] border-[#f1f1f137]'>
+          <div className=' w-[20%] rounded-lg '><img className="rounded-[30px]" src="https://firebasestorage.googleapis.com/v0/b/cootalk-e6218.appspot.com/o/profile-pwwAPPoO7CR3b9JkUPwTeSJjpSD3?alt=media&token=430d4cb2-6212-4040-bf04-1e0d0291ef43"/></div>
+          <div className=' w-[60%] text-left pl-8 '>
+            <h2 className='font-mono font-bold text-[20px] text-[#c9c2ff]'>Fazla Rabbi</h2>
+            <p className='font-mono font-bold text-[14px] text-[#c9c2ff] '>hi there</p>
+          </div>
+          <div  className=' w-[20%]'><button className=' p-2 rounded-lg font-bold text-[16px] transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 border-[1px] border-[#f1f1f160]'>Click</button></div>
+        </div>
+        <div className=' flex justify-between p-2 items-center h-22 bg-[#2f357692] rounded-[20px] border-[1px] border-[#f1f1f137]'>
+          <div className=' w-[20%] rounded-lg '><img className="rounded-[30px]" src="https://firebasestorage.googleapis.com/v0/b/cootalk-e6218.appspot.com/o/profile-pwwAPPoO7CR3b9JkUPwTeSJjpSD3?alt=media&token=430d4cb2-6212-4040-bf04-1e0d0291ef43"/></div>
+          <div className=' w-[60%] text-left pl-8 '>
+            <h2 className='font-mono font-bold text-[20px] text-[#c9c2ff]'>Fazla Rabbi</h2>
+            <p className='font-mono font-bold text-[14px] text-[#c9c2ff] '>hi there</p>
+          </div>
+          <div  className=' w-[20%]'><button className=' p-2 rounded-lg font-bold text-[16px] transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 border-[1px] border-[#f1f1f160]'>Click</button></div>
+        </div>
+        <div className=' flex justify-between p-2 items-center h-22 bg-[#2f357692] rounded-[20px] border-[1px] border-[#f1f1f137]'>
+          <div className=' w-[20%] rounded-lg '><img className="rounded-[30px]" src="https://firebasestorage.googleapis.com/v0/b/cootalk-e6218.appspot.com/o/profile-pwwAPPoO7CR3b9JkUPwTeSJjpSD3?alt=media&token=430d4cb2-6212-4040-bf04-1e0d0291ef43"/></div>
+          <div className=' w-[60%] text-left pl-8 '>
+            <h2 className='font-mono font-bold text-[20px] text-[#c9c2ff]'>Fazla Rabbi</h2>
+            <p className='font-mono font-bold text-[14px] text-[#c9c2ff] '>hi there</p>
+          </div>
+          <div  className=' w-[20%]'><button className=' p-2 rounded-lg font-bold text-[16px] transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 border-[1px] border-[#f1f1f160]'>Click</button></div>
+        </div>
+      </div>
+
+      
     </div>
   )
 }
