0a66d64a7965aa90d5fe15fb37c4764380d52167
Fazla rabbi <fazlarabbi911@gmail.com>
2024-11-04 01:28:36 +0600

    new
    haha

--- src/components/Groups.jsx
+++ src/components/Groups.jsx
@@ -1,28 +1,52 @@
 import React, { useEffect, useState } from 'react'
 import { FaLayerGroup } from "react-icons/fa";
-import { getDatabase, ref, onValue } from "firebase/database";
+import { getDatabase, ref, onValue, set, push } from "firebase/database";
+import { useSelector } from 'react-redux';
 
 
 const Groups = () => {
-    const db = getDatabase();
-    let [Groupdata,setGroupdata]= useState('')
+  const db = getDatabase();
+  let Admin = useSelector((state)=>state.storeuser.value)
 
+    let [groupdata,setgroupdata]= useState([])
     useEffect(()=>{
         const Gruopdata = ref(db, 'Groups');
         onValue(Gruopdata, (snapshot) => {
             let arry = []
           snapshot.forEach((item)=>{
-            arry.push(item.val())
+            if(item.val().AdminUid !== Admin.uid){
+              arry.push({...item.val(),grpkey:item.key})
+            }
           })
-          setGroupdata(arry)
+          setgroupdata(arry)
         });
     },[])
-    console.log(Groupdata)
+    let handleGroupRequest=(item)=>{
+      set(push(ref(db, 'GroupJoinRequest'), {
+        WhoWantJoinUid : Admin.uid,
+        WhoWantJoinName : Admin.displayName,
+        GroupName:item.GroupName,
+        GroupId :item.grpkey,
+        Adminuid :item.AdminUid,
+        AdminName :item.AdminName,
+      }));
+    }
   return (
     <div>
      <div className='h-[90%]'>
       <h2 className='font-mono font-bold text-[24px] text-[#d4cff8] bg-[#272d9859] p-2 pl-4 flex gap-2 '> <FaLayerGroup className='text-4xl text-[#386cb0]' /> Groups</h2>
-
+      {groupdata.map(item =>(
+              <div className=' flex justify-between p-2 items-center h-20 bg-[#2f357692] shadow-2xl rounded-[20px] border-[1px] border-[#f1f1f137] m-3'>
+              <div className=' w-[20%] rounded-lg h-[100%] '><img className="rounded-[20px] h-[100%] ml-2" /></div>
+              <div className=' w-[60%] text-left pl-8 '>
+                <h2 className='font-mono font-bold text-[20px] text-[#c9c2ff]'>{item.GroupName}</h2>
+                <p className='font-mono font-bold text-[14px] text-[#c9c2ff] '></p>
+              </div>
+              <div  className=' w-[17%] flex '>
+                <button onClick={()=>handleGroupRequest(item)} className='bg-slate-400 p-1.5 border-[1.3px] rounded-lg font-extralight'>Join</button>
+                  </div>
+            </div>
+      ))}
     </div>
     </div>
   )
--- src/components/MyGroup.jsx
+++ src/components/MyGroup.jsx
@@ -1,8 +1,8 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { GrUserAdmin } from "react-icons/gr";
 import { MdCreateNewFolder } from "react-icons/md";
 import { IoClose } from "react-icons/io5";
-import { getDatabase, push, ref, set } from "firebase/database";
+import { getDatabase, onValue, push, ref, remove, set } from "firebase/database";
 import { useSelector } from 'react-redux';
 
 // modal
@@ -13,9 +13,10 @@ const MyGroup = () => {
     let Admin = useSelector((state)=>state.storeuser.value)
 
     const [Modalopen, setmodalOpen] = useState(false)
+    // modal 2 grp request accept delete
+    const [GrpModalopen, setgrpmodalOpen] = useState(false)
+
     let [grpName,setgrpName]= useState('')
-
-
     const db = getDatabase();
     let handleCreatGroup =()=>{
         setmodalOpen(!Modalopen)
@@ -24,31 +25,106 @@ const MyGroup = () => {
             AdminName:Admin.displayName,
             AdminUid:Admin.uid
           }))
-
     }
     let handleGrorupname=(e)=>{
         setgrpName(e.target.value)
     }
-    // console.log(grpName)
+    // myGroup data pull ⌵ 
+    let [groupdata,setgroupdata]= useState([])
+    useEffect(()=>{
+        const Gruopdata = ref(db, 'Groups');
+        onValue(Gruopdata, (snapshot) => {
+            let arry = []
+          snapshot.forEach((item)=>{
+            if(item.val().AdminUid == Admin.uid){
+              arry.push({...item.val(),grpkey:item.key})
+            }
+          })
+          setgroupdata(arry)
+        });
+    },[])  
+     // Group Request data pull ⌵ 
+    let [GroupRequest,setGroupRequest]=useState([])
+    useEffect(()=>{
+      const starCountRef = ref(db, 'GroupJoinRequest/');
+      onValue(starCountRef, (snapshot) => {
+         let arry = []
+          snapshot.forEach((item)=>{
+            arry.push({...item.val(),GrpRequestKey:item.key})
+          })
+          setGroupRequest(arry)
+          // console.log(arry[0])
+      });
+    },[])
+    let handleRequestAccept=(item)=>{
+      set(push(ref(db,'GroupAndMembers' )), {
+          ...item
+      }).then(()=>{
+        remove(ref(db,'GroupJoinRequest/'+item.GrpRequestKey))
+      })
+      console.log(item.GrpRequestKey)
+    }
+    let handleRequestDelete=(item)=>{
+      remove(ref(db,'GroupJoinRequest/'+item.GrpRequestKey))
+      console.log(item.GrpRequestKey)
+    }
 
   return (
-    <div className=''>
-    <div className='h-[90%] bg-[#272d9859] flex justify-between'>
-      <h2 className='font-mono font-bold text-[24px] text-[#d4cff8]  p-2 pl-4 flex gap-2 '> <GrUserAdmin className='text-4xl text-[#efa954f0]' /> My Groups</h2>
-      <h2 className='font-mono font-bold text-[24px] text-[#d4cff8]  p-2 pl-4 flex gap-2 ' onClick={()=>setmodalOpen(true)}> <MdCreateNewFolder className='text-4xl text-[#83aaf7]' /> </h2>
+    <div>
+    <div className='h-[90%]'>
+      <div className='flex justify-between bg-[#272d9859]'>
+        <h2 className='font-mono font-bold text-[24px] text-[#d4cff8]  p-2 pl-4 flex gap-2 '> <GrUserAdmin className='text-4xl text-[#efa954f0]' /> My Groups</h2>
+        <h2 className='font-mono font-bold text-[24px] text-[#d4cff8]  p-2 pl-4 flex mr-[-80px] ' onClick={()=>setmodalOpen(true)}> <MdCreateNewFolder className='text-4xl text-[#83aaf7]' /> </h2>
+        <button onClick={()=>setgrpmodalOpen(!GrpModalopen)} className='bg-blue-400 px-2 rounded-lg text-[15px]'>Requests</button>
+      </div>
+      {/* modal */}
       {Modalopen &&
-        <div   className='absolute top-0 flex justify-center items-center left-0 w-full h-full  backdrop-blur-sm'>
-
+        <div className='absolute top-0 flex justify-center items-center left-0 w-full h-full  backdrop-blur-sm'>
             <div className='p-4 bg-gray-300 rounded-md relative'>
             <IoClose className='absolute top-2 right-2 text-[25px] bg-red text-red-500'onClick={()=>setmodalOpen(!Modalopen)}/>
+                              <button onClick={()=>setgrpmodalOpen(!GrpModalopen)} className='bg-blue-400 p-2 rounded-lg text-[11px]'>Request</button>
                 <h2 className='text-dark font-bold font-sans mb-2'>Creat Group </h2>
 
                 <input onChange={handleGrorupname} className='rounded-md p-1 text-black'  type="text" placeholder='group name' />
                 <button onClick={()=>handleCreatGroup()} className='block bg-blue-700 text-center w-full mt-2 rounded-lg' >Creat</button>
             </div>
         </div>
-      }
+      }{/* modal */}
+      {/* Group Request data */}
+      {groupdata.map(item =>(
+              <div className=' flex justify-between p-2 items-center h-20 bg-[#2f357692] shadow-2xl rounded-[20px] border-[1px] border-[#f1f1f137] m-3'>
+              <div className=' w-[80%] text-left pl-12 '>
+                <h2 className='font-mono font-bold text-[20px] text-[#c9c2ff]'>{item.GroupName}</h2>
+                <p className='font-mono font-bold text-[14px] text-[#c9c2ff] '></p>
+              </div>
+              <div  className=' w-[17%] flex '>
 
+              </div>
+            </div>
+      ))}
+      {/* Group Request accept delet  */}
+
+      {GrpModalopen &&
+        <div className='absolute top-0 flex justify-center items-center left-0 w-full h-full  backdrop-blur-sm'>
+            <div className='p-4  rounded-md relative'>
+            <IoClose className='absolute top-0 right-0  text-[35px] bg-red text-red-500'onClick={()=>setgrpmodalOpen(!GrpModalopen)}/>
+
+            {GroupRequest.map(item =>(
+              <div className='w-[400px] flex justify-between p-2 items-center h-20 bg-[#2f3576ea] shadow-2xl rounded-[20px] border-[1px] border-[#f1f1f137] m-3'>
+              <div className=' w-[80%] text-left pl-12 '>
+                <h2 className='font-mono font-bold text-[20px] text-[#c9c2ff]'>{item.GroupName}</h2>
+                <p className='font-mono font-bold text-[14px] text-[#c9c2ff] '>{item.WhoWantJoinName}</p>
+              </div>
+              <div  className=' flex gap-3 mr-1'>
+                <button onClick={()=>handleRequestAccept(item)} className='bg-blue-400 p-2 rounded-lg text-[11px]'>Accept</button>
+                <button onClick={()=>handleRequestDelete(item)} className='bg-red-500 p-2 rounded-lg text-[11px]'>Delete</button>
+              </div>
+            </div>
+            ))}
+
+            </div>
+        </div>
+      }
     </div>
     </div>
   )
